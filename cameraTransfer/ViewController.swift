1c195410c66ff72ba9e4d9064cbf99e9ea47ad32
Takuro <tt08310831@gmail.com>
2020-07-23 11:15:06 +0900

    Initial Commit

---
+++ cameraTransfer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cameraTransfer
+//
+//  Created by Takuro TOKUNAGA on 2020/07/23.
+//  Copyright © 2020 Takuro TOKUNAGA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
